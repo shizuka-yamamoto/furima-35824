0d33d49f4fbad3dd109e5899f0baec0366468951
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-02 14:12:08 +0900

    Userモデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname
-    validates :birth_day
-      
-      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-        validates :first_name
-        validates :last_name
-      end
+  validates :nickname, presence: true
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー]+\z/, message: ' is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー]+\z/, message: ' is invalid. Input full-width katakana characters' }
+  validates :birth_day, presence: true
 
-      with_options format: { with: /\A[ァ-ヶー]+\z/ } do
-        validates :first_name_kana
-        validates :last_name_kana
-      end
-  end
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX
-
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers'
 end
