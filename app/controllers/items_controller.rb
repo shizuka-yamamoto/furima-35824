ec3dc0d78fc1ea5dd1b9e28eb20f20798091d305
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-03 17:21:06 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name,:description, :category_id, :condition_id, :postage_id, :shipping_area_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,22 +3,24 @@ class Item < ApplicationRecord
   belongs_to :category
 	belongs_to :condition
 	belongs_to :postage
-	belongs_to :shipping_area
-	belongs_to :handling_time
-
+	belongs_to :shippingarea
+	belongs_to :handlingtime
+	belongs_to :user
+	has_one_attached :image
+	
 	with_options presence: true do
+		validates :image
     validates :item_name
     validates :description
-		validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+		validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid. Input half-width characters
+			"}
 	end
-	with_options numericality: { other_than: 1 } do
+	with_options numericality: { other_than: 1, message: "can't be blank" } do
 		validates :category_id
 		validates :condition_id
 		validates :postage_id
 		validates :shipping_area_id
 		validates :handling_time_id
 	end
-
-	belongs_to :user
 	
 end
