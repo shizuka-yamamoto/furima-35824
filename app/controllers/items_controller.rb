6d08875152e1f414e72a9a8c4f5e4725e90a85f3
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-04 06:47:38 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name,:description, :category_id, :condition_id, :postage_id, :shipping_area_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :postage_id, :shipping_area_id,
+                                 :handling_time_id, :price, :image).merge(user_id: current_user.id)
   end
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it 'priceが¥300~¥9,999,999の間であれば登録できる' do
-        @item.price >= 300 && @item.price <= 9999999
+        @item.price >= 300 && @item.price <= 9_999_999
         expect(@item).to be_valid
       end
       it 'priceが半角数値であれば登録できる' do
@@ -43,67 +43,67 @@ RSpec.describe Item, type: :model do
       it 'ユーザーが紐付いていなければ投稿できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it 'imageがなければ登録できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
-      end 
+      end
       it 'item_nameが空では登録できない' do
-        @item.item_name = ""
+        @item.item_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Item name can't be blank")
-      end 
+      end
       it 'descriptionが空では登録できない' do
-        @item.description = ""
+        @item.description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
-      end 
+      end
       it 'category_idがid:1では登録できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
-      end 
+      end
       it 'condition_idがid:1では登録できない' do
         @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
-      end 
+      end
       it 'postage_idがid:1では登録できない' do
         @item.postage_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Postage can't be blank")
-      end 
+      end
       it 'shipping_area_idがid:1では登録できない' do
         @item.shipping_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping area can't be blank")
-      end 
+      end
       it 'handling_time_idがid:1では登録できない' do
         @item.handling_time_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Handling time can't be blank")
-      end 
+      end
       it 'priceが空では登録できない' do
-        @item.price = ""
+        @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
-      end 
+      end
       it 'priceが¥299以下では登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is out of setting range")
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'priceが¥10000000以上では登録できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is out of setting range")
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'priceが全角数値であると登録できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is out of setting range")
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
     end
   end
