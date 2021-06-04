587d50e816cdac7bb9f0720cf0dd3bb9699ad1d0
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-04 11:29:29 +0900

    priceが登録できない時のテストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -105,6 +105,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
+      it 'priceが半角英数混合であると登録できない' do
+        @item.price = 'a0a0a0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
+      end
+      it 'priceが半角英語だけでは登録できない' do
+        @item.price = 'aaaaaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
+      end
     end
   end
 end
