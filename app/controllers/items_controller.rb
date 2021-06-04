0795c7a42a4792f0d336c81db58c126f7d6c918b
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-04 17:37:38 +0900

    URLを直接入力しても編集ページに遷移できないように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,11 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if @item.user == current_user
+      render :edit
+    else
+      redirect_to root_path
+    end
   end
 
   def update
