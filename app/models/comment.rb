b2f04faf3524a7fc07bee73a7accababf2f0cbe7
Shizuka Yamamoto <shiiishim920@gmail.com>
2021-06-15 20:03:56 +0900

    Rubocopの実行

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-	belongs_to :user
-	belongs_to :item
+  belongs_to :user
+  belongs_to :item
 
-	validates :text, presence: true
+  validates :text, presence: true
 end
